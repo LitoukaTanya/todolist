4f6ea90b467d36f7ba36b721cd8f28c281865025
Tanya <litovkotanya@gmail.com>
2024-07-06 14:42:39 +0300

    add Get all users (GET)

--- users/urls.py
+++ users/urls.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 from django.urls import path
-from .views import user_create
+from .views import user_create, UserList
 
 app_name = 'users'
 
 urlpatterns = [
     path('create/', user_create, name='create'),
+    path('listusers/', UserList.as_view(), name='listusers',)
 
 ]
--- users/views.py
+++ users/views.py
@@ -1,5 +1,6 @@
+from django.contrib.auth.models import User
 from django.shortcuts import render
-from rest_framework import status
+from rest_framework import status, generics
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .serializers import UserSerializer
@@ -13,3 +14,8 @@ def user_create(request):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class UserList(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
