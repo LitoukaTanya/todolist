3a06fcb9100f2da4f22a199bd0b4bdf23280a90c
Tanya <litovkotanya@gmail.com>
2024-07-08 17:16:43 +0300

    register model in admin.py

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Category, Priority, Task
 
-# Register your models here.
+admin.site.register(Category)
+admin.site.register(Priority)
+admin.site.register(Task)
