78333786e299a26924b8a5be3f2db7c3ace6c1af
Tanya <litovkotanya@gmail.com>
2024-07-08 19:31:10 +0300

    add functional: Get all tasks for user

--- app/urls.py
+++ app/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from app.views import TaskCreateView
+from app.views import TaskCreateView, TaskListView
 
 urlpatterns = [
     path('create/', TaskCreateView.as_view(), name='task_create'),
+    path('listtask/', TaskListView.as_view(), name='task_list'),
 
 ]
--- app/views.py
+++ app/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
-from rest_framework import status
+from rest_framework import status, generics
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
+from app.models import Task
 from app.serializers import TaskSerializer
 
 
@@ -14,3 +16,11 @@ class TaskCreateView(APIView):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+
+class TaskListView(generics.ListAPIView):
+    serializer_class = TaskSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+        return Task.objects.filter(created_by=user, deleted=False)
